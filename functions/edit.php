e16fa21e364a49bebce9116323296a6bdf3cb813
InoferioCharls <148921604+InoferioCharls@users.noreply.github.com>
2023-11-02 00:33:30 +0800

    error

--- functions/edit.php
+++ functions/edit.php
@@ -1,9 +1,9 @@
 <?php 
     include_once("../dbConnection/mysqlconfig_connection.php");
     if(isset($_POST["update"])){
-        $id = $_POST["id"];
-        $code = $_POST["code"];
-        $name = $_POST["name"];
+        $id = $_POST['id'];
+        $code = $_POST['code'];
+        $name = $_POST['name'];
 
         if(empty($code)||empty($name)){
             if(empty($code)){
