3c2320426db7cbdb645e6f8dd88eb39b9b379a3a
InoferioCharls <148921604+InoferioCharls@users.noreply.github.com>
2023-11-01 23:31:38 +0800

    missing

--- dbConnection/mysqlconfig_connection.php
+++ dbConnection/mysqlconfig_connection.php
@@ -4,7 +4,7 @@
     DEFINE('DB_HOST','localhost');
     DEFINE('DB_NAME','dbschedule');
 
-        $dbc = mysqli_connect(DB_USER,DB_PASSWORD,DB_HOST,DB_NAME )
+        $dbc = mysqli_connect(DB_USER,DB_PASSWORD,DB_HOST,DB_NAME );
         or die('Could not connect to MySQL: '. mysqli_connect_error());
 
 ?>
