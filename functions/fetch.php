d77a802b7977420272ee4f119102ea008eb12f4a
InoferioCharls <148921604+InoferioCharls@users.noreply.github.com>
2023-11-28 20:22:46 +0800

    right

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,4 +1,4 @@
 <?php 
-    $query =" select * FROM tblsubjects left join tblsyllabus on tblsubjects.Subject_ID = tblsyllabus.Subject_ID;";
+    $query =" select * FROM tblsubjects right join tblsyllabus on tblsubjects.Subject_ID = tblsyllabus.Subject_ID;";
     $result =mysqli_query($dbc, $query);
 ?>
