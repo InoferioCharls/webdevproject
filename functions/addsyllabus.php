809a9aa100c3acf7f5bc3159687dd923eaa33f60
InoferioCharls <148921604+InoferioCharls@users.noreply.github.com>
2023-11-28 17:34:04 +0800

    edit

--- functions/addsyllabus.php
+++ functions/addsyllabus.php
@@ -12,20 +12,21 @@
     include_once("../dbConnection/mysqlconfig_connection.php");
 
     if (isset($_POST['submit'])) {
-        $scode = $_POST['syllabus_code'];
-        $sauthor = $_POST['syllabus_author'];
+        $code = $_POST['code'];
+        $author = $_POST['syllabus_author'];
+        $subject = $_POST['subject'];
 
-        if (empty($scode) || empty($sauthor)) {
-            if (empty($scode)) {
+        if (empty($code) || empty($author)) {
+            if (empty($code)) {
                 echo "<font color = 'red'> object code field is empty.</font<br>";
             }
 
-            if (empty($sauthor)) {
+            if (empty($author)) {
                 echo "<font color = 'red'> object name field is empty.</font<br>";
             }
             echo "<br><a href='javascript:self.history.back();'>Go Back</a>";
         } else {
-            $result = mysqli_query($dbc, "INSERT INTO tblsyllabus(syllabus_code, syllabus_author) VALUES('$scode', '$sauthor')");
+            $result = mysqli_query($dbc, "INSERT INTO tblsyllabus(syllabus_code, syllabus_author) VALUES('$code', '$author')");
 
             echo "<font color='green'> Data added successfully.";
             echo "<br><a href='../index.php'>View Result</a>";
